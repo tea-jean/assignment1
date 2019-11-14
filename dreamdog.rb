98b2186943e1baa0c8ccfb570879cdd1de9a49b4
tea-jean <tjholt@live.com.au>
2019-11-14 14:02:43 +1100

    added dreamdog.rb with class Breed workings

---
+++ dreamdog.rb
@@ -0,0 +1,19 @@
+class Breed
+    attr_accessor :active_score. :size_score, :coat_score, :ear_type
+    
+    def initialize (active_score, size_score, coat_score, ear_type)
+        @active_score = active_score
+        @size_score = size_score
+        @coat_score = coat_score
+        @ear_type = ear_type
+    end
+
+    def active_score
+        @active_score
+    end
+
+    def size_score  
+        @
+end
+
+labrador = Breed.new()
