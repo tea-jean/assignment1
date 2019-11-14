4a60328b9dd25aa3a3a66a6548c128cc54a5428c
tea-jean <tjholt@live.com.au>
2019-11-14 17:42:31 +1100

    Up to method for breed_choice

--- dreamdog.rb
+++ dreamdog.rb
@@ -1,6 +1,6 @@
 active = ['low', 'medium', 'high']
-ears = ['drop', 'rose', 'uppy']
-coat = ['short', 'med', 'long']
+ears = ['droopy', 'rose', 'uppy']
+coat = ['short', 'medium', 'long']
 
 class Breed
     attr_accessor :activity_level, :ear_type, :coat_type 
@@ -22,5 +22,45 @@ dalmation = Breed.new("high", "droop", "short")
 husky = Breed.new("high", "uppy", "medium")
 aussie_shepherd = Breed.new("high", "drop", "long")
 
-p labrador.activity_level
+arr = [""]
 
+puts "Welcome to your Dream Dog selector"
+puts "Please start by telling me the activity level of your ultimate dog: "
+
+active.each do |level|
+    puts level
+end
+
+activity_selection = gets.chomp
+arr.push (activity_selection)
+
+puts "How to you envision your future pups ears?"
+
+ears.each do |ear|
+    puts ear
+end
+
+ear_selection = gets.chomp
+arr.push (ear_selection)
+
+puts "What type of coat do you wish your dog to have?"
+
+coat.each do |coat|
+    puts coat
+end
+
+coat_selection = gets.chomp + " coat"
+arr.push (coat_selection)
+
+puts "Your Dream dog has a #{activity_selection} activity level. She has beautiful #{ear_selection} ears. Her #{coat_selection} is shiny and soft." 
+
+def breed_choice
+    when
+    @arr == Breed
+    #compare arr with Breed instances to choose most 'matching' instance. 
+    #How to iterate over each instance of Breed?
+    end
+end
+puts breed_choice
+
+puts" We suggest that your Dream Dog is a ..."
