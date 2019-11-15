2091685308a00d46db3218242182e42f43982292
tea-jean <tjholt@live.com.au>
2019-11-15 11:30:53 +1100

    added comments for changes in dreamdog.rb

--- dreamdog.rb
+++ dreamdog.rb
@@ -1,6 +1,6 @@
-active = ['low', 'medium', 'high']
-ears = ['droopy', 'rose', 'uppy']
-coat = ['short', 'medium', 'long']
+active = ['1-low', '2-medium', '3-high']
+ears = ['1-droop', '2-rose', '3-uppy']
+coat = ['1-short', '2-medium', '3-long']
 
 class Breed
     attr_accessor :activity_level, :ear_type, :coat_type 
@@ -12,55 +12,59 @@ class Breed
     end
 end
 
-bulldog = Breed.new("low", "rose", "short")
+@bulldog = Breed.new(1,2,1)
 clumber = Breed.new("low", "droop", "medium")
-pedingese = Breed.new("low", "droop", "long")
+pekingese = Breed.new("low", "droop", "long")
 whippet = Breed.new("medium", "rose", "short")
 akita = Breed.new("medium", "uppy", "medium")
 bernese = Breed.new("medium", "droop", "long")
 dalmation = Breed.new("high", "droop", "short")
 husky = Breed.new("high", "uppy", "medium")
-aussie_shepherd = Breed.new("high", "drop", "long")
+aussie_shepherd = Breed.new("high", "droop", "long")
 
-arr = [""]
+@arr = []
 
 puts "Welcome to your Dream Dog selector"
-puts "Please start by telling me the activity level of your ultimate dog: "
+puts "Please start by selecting a number for the activity level of your ultimate dog: "
 
 active.each do |level|
     puts level
 end
 
 activity_selection = gets.chomp
-arr.push (activity_selection)
+#A method in here to convert user input to a number?
+@arr.push (activity_selection)
 
-puts "How to you envision your future pups ears?"
+puts "How to you envision your future pups ears? Select a number:"
 
 ears.each do |ear|
     puts ear
 end
 
 ear_selection = gets.chomp
-arr.push (ear_selection)
+@arr.push (ear_selection)
 
-puts "What type of coat do you wish your dog to have?"
+puts "What type of coat do you wish your dog to have? Enter your selection with a number:"
 
 coat.each do |coat|
     puts coat
 end
 
-coat_selection = gets.chomp + " coat"
-arr.push (coat_selection)
+coat_selection = gets.chomp
+@arr.push (coat_selection)
 
-puts "Your Dream dog has a #{activity_selection} activity level. She has beautiful #{ear_selection} ears. Her #{coat_selection} is shiny and soft." 
+puts "Your Dream dog has a #{activity_selection} activity level. On their head are beautiful #{ear_selection} ears. Their #{coat_selection} is shiny and soft." 
 
 def breed_choice
-    when
-    @arr == Breed
+    if @arr == @bulldog
+        puts "bulldog"
+        
     #compare arr with Breed instances to choose most 'matching' instance. 
     #How to iterate over each instance of Breed?
+    #change inputs into numbers? for easier comparison?
     end
 end
 puts breed_choice
+puts @arr
 
-puts" We suggest that your Dream Dog is a ..."
+puts" We suggest that your Dream Dog is a #{breed_choice}..."
